194ce73468dcc8df1d59445d450e45f2873db3be
Gekko Wrld <gekkowrld@gmail.com>
2024-02-06 22:46:56 +0300

    chore: Append data to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Append to a file.
+
+No checks should be done on the file.
+"""
+
+def append_write(filename="", text=""):
+    """Append text to filename"""
+    with open(filename, "a", encoding="UTF-8") as f:
+        return f.write(text)
+
