7f9dfdde7e87e181faa327f2941d5f3fca53d3b4
Gekko Wrld <gekkowrld@gmail.com>
2024-02-06 23:02:11 +0300

    fix: Change file organization

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-import json
-
 """
 Convert from JSON to sting.
 
 No exceptions are handled.
 """
 
+import json
+
 def to_json_string(my_obj):
     """Juts dump the object"""
-
     return json.dumps(my_obj)
 
