a15db6b61b5ef47f11b34645eca29871e2050be9
Gekko Wrld <gekkowrld@gmail.com>
2024-02-06 23:52:43 +0300

    chore: Save args to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Add arguments to a list then save them in a file.
+
+No exceptions/errors will be handled
+"""
+
+from sys import argv
+import json
+
+def add_item(filename, args):
+    """Add items to a file"""
+    save_to_json = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
+    data_to_be_saved = []
+    try:
+        data_to_be_saved = load_from_json(filename)
+        data_to_be_saved[len(data_to_be_saved):] = args
+    except FileNotFoundError:
+        pass
+
+    save_to_json(data_to_be_saved, filename)
+
+if __name__ == "__main__":
+    filename = "add_item.json"
+    args = argv[1:]
+    add_item(filename, args)
