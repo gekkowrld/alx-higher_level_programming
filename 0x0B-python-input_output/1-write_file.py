2aa8160a118b439f00ded2df0e5ae48d46276580
Gekko Wrld <gekkowrld@gmail.com>
2024-02-06 22:36:43 +0300

    chore: Write data to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+Write to file and return
+the number of characters written
+"""
+
+def write_file(filename="", text=""):
+    """Write text and return the data written"""
+    with open(filename,'w', encoding="UTF-8") as f:
+        return f.write(text)
+
