4173e5ea1f233b0d0952fa0b08086d4a8b261d40
Gekko Wrld <gekkowrld@gmail.com>
2024-02-06 23:27:46 +0300

    chore: Load JSON data from a file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Get JSON from a file
+
+No Error checking is done.
+"""
+
+import json
+
+def load_from_json_file(filename):
+    """Load the data from specified file"""
+    with open(filename, encoding="UTF-8") as f:
+        return json.loads(f.read())
+
